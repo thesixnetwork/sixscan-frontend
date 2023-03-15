cc6521b4a1bd662f090c2e1c4376691c2eb6482b
jonathan-six <97500179+jonathan-six@users.noreply.github.com>
2023-03-15 17:23:25 +0700

    🐛 (ENV): fix chain name bug

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  env: {
+    NEXT_PUBLIC_CHAIN_NAME: process.env.NEXT_PUBLIC_CHAIN_NAME,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
