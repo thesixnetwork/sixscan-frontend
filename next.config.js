d50ae35b2dbd60919e5c12cdd55ba10bfd701cf2
jonathan-six <97500179+jonathan-six@users.noreply.github.com>
2023-03-15 17:32:17 +0700

    🚧 (ENV): fix next_public problem

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   env: {
-    NEXT_PUBLIC_CHAIN_NAME: process.env.NEXT_PUBLIC_CHAIN_NAME,
+    CHAIN_NAME: process.env.NEXT_PUBLIC_CHAIN_NAME,
   },
 };
 
