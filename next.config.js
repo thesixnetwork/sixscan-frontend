87e1079bdb67b3c83960bfaed6f128729ad09f32
jonathan-six <97500179+jonathan-six@users.noreply.github.com>
2023-03-15 17:38:15 +0700

    🐛 (env): fix next_public not working

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    CHAIN_NAME: process.env.NEXT_PUBLIC_CHAIN_NAME,
-  },
 };
 
 module.exports = nextConfig;
